eccf6c05d161ec9348d32ea1985f8c88d4c34c81
feroxx <frhatfrat@gmail.com>
2015-05-17 15:49:09 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -37,8 +37,7 @@ device/gionee/e7/ueventd.qcom.rc:root/ueventd.qcom.rc
 PRODUCT_COPY_FILES += \
 device/gionee/e7/hsic.control.bt.sh:system/etc/hsic.control.bt.sh \
 device/gionee/e7/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-device/gionee/e7/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
-device/gionee/e7/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh
+device/gionee/e7/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
