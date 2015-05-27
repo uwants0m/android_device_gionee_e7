dd1a4abf8f7f772a9a501f606751f743f4b0ad06
feroxx <frhatfrat@gmail.com>
2015-05-27 04:21:40 +0300

    Update omni_e7.mk

--- omni_e7.mk
+++ omni_e7.mk
@@ -23,6 +23,9 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 720x480
+
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
