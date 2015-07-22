91a63cb38c60dcaa4d443ff6d010def742f9430f
feroxx <frhatfrat@gmail.com>
2015-07-22 09:06:01 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,3 +147,5 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.148/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+
+PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
