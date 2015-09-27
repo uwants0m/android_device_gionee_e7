0b53b6cea75af3a4fd7d71cc55bd2246499e5ead
feroxx <frhatfrat@gmail.com>
2015-09-27 14:52:48 +0300

    Update e7.mk

--- e7.mk
+++ e7.mk
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # TWRP
 PRODUCT_COPY_FILES += \
     device/gionee/e7/rootdir/init.qcom.usb.rc:recovery/root/init.usb.rc \
