dab0f61cd8b0e48d46cd59d5018f7db2496efc4f
feroxx <frhatfrat@gmail.com>
2016-01-04 19:58:32 +0200

    Update Android.mk

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -32,7 +32,7 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../util
 
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display/msm8974/libgralloc
+        hardware/qcom/display-caf-bfam/libgralloc
 
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
