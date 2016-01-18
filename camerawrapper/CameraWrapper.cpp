9f20840fde1a6107d78b8b797012eb779ac55791
feroxx <frhatfrat@gmail.com>
2016-01-18 11:05:50 +0200

    Update CameraWrapper.cpp

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -111,7 +111,6 @@ static char *camera_fixup_getparams(int __attribute__((unused)) id,
 
     params.set(android::CameraParameters::SCENE_MODE_GESTURE, "gesture");
     params.set(android::CameraParameters::SCENE_MODE_FOOD, "food");
-    params.set(android::CameraParameters::getstr, "getstr");
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
@@ -136,7 +135,6 @@ static char *camera_fixup_setparams(int id, const char *settings)
 
     params.set(android::CameraParameters::SCENE_MODE_GESTURE, "gesture");
     params.set(android::CameraParameters::SCENE_MODE_FOOD, "food");
-    params.set(android::CameraParameters::getstr, "getstr");
 
 
     android::String8 strParams = params.flatten();
