f8dc03ad4c4d65bb56c718b2b3a5dfa37bfef85a
feroxx <frhatfrat@gmail.com>
2016-01-22 11:01:43 +0200

    Update CameraParametersExtra.h

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -17,15 +17,9 @@
 #define CAMERA_PARAMETERS_EXTRA_C \
 const char CameraParameters::SCENE_MODE_GESTURE[] = "gesture"; \
 const char CameraParameters::SCENE_MODE_FOOD[] = "food"; \
-int CameraParameters::getStr(const char *key) const { return -1; }; \
-int QCamera2HardwareInterface::gnupdatePictureFrame(); \
-int QCamera2HardwareInterface::gnupdateFeatureParameters(); \
-int QCamera2HardwareInterface::updateParameters(const char *parms, bool &needRestart) const { return -1; };
+int CameraParameters::getStr(const char *key) const { return -1; };
  
 #define CAMERA_PARAMETERS_EXTRA_H \
     static const char SCENE_MODE_GESTURE[]; \
     static const char SCENE_MODE_FOOD[]; \
-    int getStr(const char *key) const; \
-    int QCamera2HardwareInterface::gnupdatePictureFrame; \
-    int QCamera2HardwareInterface::gnupdateFeatureParameters; \
-    int QCamera2HardwareInterface::updateParameters(const char *parms, bool &needRestart) const;
+    int getStr(const char *key) const;
