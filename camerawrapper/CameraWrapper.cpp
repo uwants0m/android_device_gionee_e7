919a2e7f437ac54cf12b88648f78e522f84e864d
feroxx <frhatfrat@gmail.com>
2016-01-31 20:04:40 +0200

    Update CameraWrapper.cpp

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -109,17 +109,6 @@ static char *camera_fixup_getparams(int __attribute__((unused)) id,
     params.dump();
 #endif
 
-    /* Convert to Qualcomm-style max parameters */
-    if (params.get("contrast-max")) {
-        params.set("max-contrast", params.get("contrast-max"));
-    }
-    if (params.get("saturation-max")) {
-        params.set("max-saturation", params.get("saturation-max"));
-    }
-    if (params.get("sharpness-max")) {
-        params.set("max-sharpness", params.get("sharpness-max"));
-    }
-
     params.set(android::CameraParameters::SCENE_MODE_GESTURE, "gesture");
     params.set(android::CameraParameters::SCENE_MODE_FOOD, "food");
 
@@ -144,17 +133,10 @@ static char *camera_fixup_setparams(int id, const char *settings)
     params.dump();
 #endif
 
-    /* Convert to Qualcomm-style max/min parameters */
-    if (params.get("max-contrast")) {
-        params.set("contrast-max", params.get("max-contrast"));
-    }
-    if (params.get("max-saturation")) {
-        params.set("saturation-max", params.get("max-saturation"));
-    }
-    if (params.get("max-sharpness")) {
-        params.set("sharpness-max", params.get("max-sharpness"));
-    }
- 
+    params.set(android::CameraParameters::SCENE_MODE_GESTURE, "gesture");
+    params.set(android::CameraParameters::SCENE_MODE_FOOD, "food");
+
+
     android::String8 strParams = params.flatten();
 
     if (fixed_set_params[id])
