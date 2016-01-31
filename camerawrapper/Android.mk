1bcf0777bb1744bc63088c94c3a361f3ed97b91e
feroxx <frhatfrat@gmail.com>
2016-01-31 21:35:57 +0200

    Update Android.mk

--- camerawrapper/Android.mk
+++ camerawrapper/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES += \
+LOCAL_C_INCLUDES := \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
@@ -11,8 +11,15 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+# workaround for hardware/qcom/camera already defining camera.board
+LOCAL_MODULE := camera-wrapper.msm8974
+
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+
+$(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE) | $(ACP)
+	@echo "Install (overridden): $@"
+	$(hide) $(ACP) -fp $< $(dir $@)/camera.msm8974$(TARGET_SHLIB_SUFFIX)
